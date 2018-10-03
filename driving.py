8ed64ece515ff8d84cbeb7e72a6a809babba4ba8
PIPIDADA <archer021010@gmail.com>
2018-10-03 10:22:39 +0800

    add American

--- driving.py
+++ driving.py
@@ -2,8 +2,14 @@ country = input('請問您是哪國人？')
 age = input('請輸入您的年齡：')
 age = int(age)
 if country == '台灣':
-    if age >= 1 and age < 18:
-    	print('您目前無法考取駕照！')
+    if age >= 18:
+        print('您可以考駕照了！')
     else:
-    	print('您可以考駕照了！')
+        print('您目前無法考取駕照！')
+elif country == 'American':
+    if age >= 16:
+        print("You can try to get a driver's license!!")
+    else:
+        print("Sorry, you can't drive.") 
+        print("To ride a bike is good for you!")
 
